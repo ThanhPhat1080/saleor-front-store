e3a4aaaeb7a637482c880918d44824a27d51fb2e
Phat Truong <thanhphat.994@gmail.com>
2023-09-12 22:20:44 +0700

    Config tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,7 @@ export default function App() {
     return (
         <html lang="en">
             <head>
-                <Meta />
+                <Meta  />
                 <Links />
             </head>
             <body className="">
